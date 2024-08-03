8946884363b8c9e37d72afdb30d10127fab7abc2
Kevin Alavik <kevin@alavik.se>
2024-08-03 17:47:17 +0200

    feat: Added framebuffer test

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <sphynxboot.h>
 
 void hlt()
 {
@@ -7,22 +8,23 @@ void hlt()
         ;
 }
 
-void outb(uint16_t port, uint8_t value) {
-    __asm__ volatile("outb %1, %0" : : "dN"(port), "a"(value));
+void putpixel(framebuffer_t *fb, uint32_t x, uint32_t y, uint8_t r, uint8_t g, uint8_t b) {
+    if (x >= fb->width || y >= fb->height) {
+        return;
+    }
+
+    uint32_t *pixel_addr = (uint32_t *)(fb->address + y * fb->pitch + x * (fb->bpp / 8));
+
+    uint32_t color = 0xFF000000 | (r << 16) | (g << 8) | b;
+
+    *pixel_addr = color;
 }
 
-void putc(char ch) {
-    outb(0xE9, ch);
-}
-
-void print(const char* s) {
-    while (*s) 
-        outb(0xe9, *s++);
-} 
-
-void _start() {
-    putc('\033');
-    putc('c');
-    print("info: Kernel loaded");
+void _start(framebuffer_t *fb) {
+    for(int x = 0; x < 100; x++) {
+        for(int y = 0; y < 100; y++) {
+            putpixel(fb, x, y, 255, 255, 255);
+        }
+    }
     hlt();
 }
